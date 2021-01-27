f4465ff31adb299e5f263866958361f48b20eb44
Bruna Guedes <bruna.guedes02@gmail.com>
2021-01-27 14:47:30 -0300

    aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import Head from '../src/components/Head'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import Head from '../src/components/Head';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -19,6 +21,8 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
   return (
     <>
       <Head title={db.title} bg={db.bg} description={db.description} />
@@ -30,7 +34,25 @@ export default function Home() {
               <h1>{db.title}</h1>
             </Widget.Header>
             <Widget.Content>
-              <p>{db.description}</p>
+              <form onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                console.log('Fazendo uma submissão por meio do react');
+              }}
+              >
+                <input
+                  onChange={function (infosDoEvento) {
+                    console.log(infosDoEvento.target.value);
+                    // State
+                    // name = infosDoEvento.target.value;
+                    setName(infosDoEvento.target.value);
+                  }}
+                  placeholder="Diz ai seu nome"
+                />
+                <button type="submit" disabled={name.length === 0}>
+                  Jogar
+                </button>
+              </form>
             </Widget.Content>
           </Widget>
 
--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -1,22 +1,28 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import React from 'react';
 
+// eslint-disable-next-line react/prop-types
 export default function Home({ title, description, bg }) {
   return (
-      <Head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:locale" content="pt_BR" />
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-        <meta property="og:title" content={title} />
-        <meta property="og:site_name" content="TheOfficeQuiz" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
 
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={bg} />
-        <meta property="og:image:type" content="image/jpg" />
-        <meta property="og:image:width" content="800" />
-        <meta property="og:image:height" content="600" />
-        <meta property="og:type" content="website"></meta>
-      </Head>
-  )
+      <meta property="og:locale" content="pt_BR" />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:site_name" content="TheOfficeQuiz" />
+
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={bg} />
+      <meta property="og:image:type" content="image/jpg" />
+      <meta property="og:image:width" content="800" />
+      <meta property="og:image:height" content="600" />
+      <meta property="og:type" content="website" />
+    </Head>
+  );
 }
